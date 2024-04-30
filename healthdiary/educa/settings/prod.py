d808bfd9784b0aa924645df1e0e02e2ee7de925c
leekeunha <leetoya@naver.com>
2024-04-30 18:29:05 +0900

    docker working well

--- healthdiary/educa/settings/prod.py
+++ healthdiary/educa/settings/prod.py
@@ -4,7 +4,7 @@ DEBUG = False
 ADMINS = [
     ('leekeunha','leetoya@naver.com'),
 ]
-ALLOWED_HOSTS=['healthdiary.com','www.healthdiary.com']
+ALLOWED_HOSTS=['*']
 DATABASES = {
     'default':{
         'ENGINE':'django.db.backends.postgresql',
